c4a94c5b619cd17198ebfdc87171289405555812
Tam Do <phuongtam1072@gmail.com>
2022-05-07 16:51:52 +0700

    Create game function and fix bugs

--- index.js
+++ index.js
@@ -4,43 +4,50 @@ function computerPlay() {
     return array[randomIndex]
 
 }
-let computerSelection = computerPlay().toUpperCase();
-
-let playerSelection=prompt("What do you choose? Rock, Paper, Scissors? ").toUpperCase();
-
 function playRound(player, computer) {
     if (player == "ROCK") { 
         if (computer == "SCISSORS") {
-            alert (`You win the game! Rock beats Scissors`)
+            console.log(`You win the game! Rock beats Scissors`)
         }
         else if (computer == "ROCK") {
-            alert (`It's a draw`)
+            console.log(`It's a draw`)
         }
         else if (computer == "Paper") {
-            alert (`You lose the game! Paper beats Rock`)
+            console.log(`You lose the game! Paper beats Rock`)
         }
     }
     else if (player == "SCISSORS") { 
         if (computer == "SCISSORS") {
-            alert (`It's a draw`)
+            console.log(`It's a draw`)
         }
         else if (computer == "ROCK") {
-            alert (`You lose the game! Rock beats Scissors`)
+            console.log(`You lose the game! Rock beats Scissors`)
         }
         else if (computer == "PAPER") {
-            alert (`You win the game! Scissors beats Paper`)
+            console.log(`You win the game! Scissors beats Paper`)
         }
     }
     else if (player == "PAPER") { 
         if (computer == "SCISSORS") {
-            alert (`You lose the game! Scissors beats Paper`)
+            console.log(`You lose the game! Scissors beats Paper`)
         }
         else if (computer == "ROCK") {
-            alert (`You win the game! Paper beats Rock`)
+            console.log(`You win the game! Paper beats Rock`)
         }
         else if (computer == "Paper") {
-            alert (`It's a draw`)
+            console.log(`It's a draw`)
         } 
     }
     
 }
+
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay().toUpperCase();
+        let playerSelection=prompt("What do you choose? Rock, Paper, Scissors? ").toUpperCase();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
