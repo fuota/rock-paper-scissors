da594f34b2a490167e04587d0529e50cbf2fe774
Tam Do <phuongtam1072@gmail.com>
2022-05-07 16:42:00 +0700

    Create playRound function

--- index.js
+++ index.js
@@ -4,4 +4,43 @@ function computerPlay() {
     return array[randomIndex]
 
 }
-console.log(computerPlay())
+let computerSelection = computerPlay().toUpperCase();
+
+let playerSelection=prompt("What do you choose? Rock, Paper, Scissors? ").toUpperCase();
+
+function playRound(player, computer) {
+    if (player == "ROCK") { 
+        if (computer == "SCISSORS") {
+            alert (`You win the game! Rock beats Scissors`)
+        }
+        else if (computer == "ROCK") {
+            alert (`It's a draw`)
+        }
+        else if (computer == "Paper") {
+            alert (`You lose the game! Paper beats Rock`)
+        }
+    }
+    else if (player == "SCISSORS") { 
+        if (computer == "SCISSORS") {
+            alert (`It's a draw`)
+        }
+        else if (computer == "ROCK") {
+            alert (`You lose the game! Rock beats Scissors`)
+        }
+        else if (computer == "PAPER") {
+            alert (`You win the game! Scissors beats Paper`)
+        }
+    }
+    else if (player == "PAPER") { 
+        if (computer == "SCISSORS") {
+            alert (`You lose the game! Scissors beats Paper`)
+        }
+        else if (computer == "ROCK") {
+            alert (`You win the game! Paper beats Rock`)
+        }
+        else if (computer == "Paper") {
+            alert (`It's a draw`)
+        } 
+    }
+    
+}
